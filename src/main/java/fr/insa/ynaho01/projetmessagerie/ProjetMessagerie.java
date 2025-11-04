bef2d744a461dfcf154ac870ef41cb300c4ee15f
ynaho02 <mariemichelenaho29@gmail.com>
2025-11-04 10:45:21 +0100

    first

---
+++ src/main/java/fr/insa/ynaho01/projetmessagerie/ProjetMessagerie.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package fr.insa.ynaho01.projetmessagerie;
+
+/**
+ *
+ * @author marie
+ */
+public class ProjetMessagerie {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
